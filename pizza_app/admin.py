75a753cd052217fa98b9b264da052e153bdea080
0k1nd <akindtyrbo@gmail.com>
2024-11-25 13:07:27 +0300

    all models created

--- pizza_app/admin.py
+++ pizza_app/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.contrib.admin import ModelAdmin
-from .models import Supplier, Product, Pizzaplace, Warehouse, WarehouseProduct
+from .models import Supplier, Product, Pizzaplace, Warehouse, WarehouseProduct, Order, OrderProduct, Sale, SaleProduct
 
 @admin.register(Supplier)
 class SupplierAdmin(ModelAdmin):
@@ -22,5 +22,19 @@ class WarehouseAdmin(ModelAdmin):
 class WarehouseProductAdmin(ModelAdmin):
     pass
 
+@admin.register(Order)
+class OrderAdmin(ModelAdmin):
+    pass
 
+@admin.register(OrderProduct)
+class OrderProductAdmin(ModelAdmin):
+    pass
+
+@admin.register(Sale)
+class SaleAdmin(ModelAdmin):
+    pass
+
+@admin.register(SaleProduct)
+class SaleProductAdmin(ModelAdmin):
+    pass
 # Register your models here.
--- pizza_app/models.py
+++ pizza_app/models.py
@@ -1,5 +1,6 @@
+from django.utils import timezone
 from django.db import models
-
+from django.contrib.contenttypes.models import ContentType
 
 class Supplier(models.Model):
     name = models.CharField(max_length=100)
@@ -58,3 +59,51 @@ class WarehouseProduct(models.Model):
 
     def __str__(self):
         return f'{self.id} {self.product.name} {self.quantity}'
+
+STATUS_CHOICES =(
+    ('pending', "pending"),
+    ('completed', "completed"),
+    ('canceled', "canceled"),
+)
+
+class Order(models.Model):
+    pizzaplace = models.ForeignKey(Pizzaplace,on_delete=models.CASCADE, related_name='orders')
+    status = models.CharField(max_length=150, choices=STATUS_CHOICES) #общую стоимость в сериализаторы и вью
+    date = models.DateTimeField(default=timezone.now)
+
+
+class OrderProduct(models.Model):
+    order = models.ForeignKey(Order, on_delete=models.CASCADE, related_name="order_product")
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name="product_order")
+    quantity = models.PositiveIntegerField(default=0)
+
+    def __str__(self):
+        return f'{self.id} {self.product.name} {self.quantity}'
+
+class Sale(models.Model):
+    pizzaplace = models.ForeignKey(Pizzaplace,on_delete=models.CASCADE, related_name='sales')
+    date = models.DateTimeField(default=timezone.now) #общую стоимость в сериализаторы и вью
+
+class SaleProduct(models.Model):
+    order = models.ForeignKey(Sale, on_delete=models.CASCADE, related_name="sale_product")
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name="product_sale")
+    quantity = models.PositiveIntegerField(default=0)
+
+    def __str__(self):
+        return f'{self.id} {self.product.name} {self.quantity}'
+
+TYPE_CHOICES =(
+    ('low stock level', "low stock level"),
+    ('expiring date', "expiring date"),
+)
+
+STATUS_NOTIFICATIONS_CHOICES =(
+    ('read', "read"),
+    ('unread', "unread"),
+)
+
+class Notification(models.Model):
+    type = models.CharField(max_length=150, choices=TYPE_CHOICES)
+    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
+    status = models.CharField(max_length=150, choices=STATUS_NOTIFICATIONS_CHOICES, default='unread')
+    date = models.DateTimeField(default=timezone.now)
